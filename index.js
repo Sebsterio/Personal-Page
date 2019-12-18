68d30264399cda4091591cce3d5b995e18ce27b2
Sebsterio <sebastian.rosloniec@gmail.com>
2019-12-18 14:45:56 +0100

    link index.js in index.html

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const JS30folders = [
 	"01 - JavaScript Drum Kit",
 	"02 - JS and CSS Clock",
 	"03 - CSS Variables",
-	"03 - CSS Variables",
 	"05 - Flex Panel Gallery",
 	"06 - Type Ahead",
 	"08 - Fun with HTML5 Canvas",

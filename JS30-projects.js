8fadf9c01e78c8429dd9529041b30849e6fa8c1a
Sebsterio <sebastian.rosloniec@gmail.com>
2019-12-19 18:03:35 +0100

    unhide projects

--- JS30-projects.js
+++ JS30-projects.js
@@ -1,8 +1,7 @@
 const JS30folders = [
 	"01 - JavaScript Drum Kit",
 	"02 - JS and CSS Clock",
-	"03 - CSS Variables"
-	/*
+	"03 - CSS Variables",
 	"05 - Flex Panel Gallery",
 	"06 - Type Ahead",
 	"08 - Fun with HTML5 Canvas",
@@ -10,7 +9,6 @@ const JS30folders = [
 	"12 - Key Sequence Detection",
 	"13 - Slide in on Scroll",
 	"16 - Mouse Move Shadow"
-	*/
 ];
 
 const JS30projects = JS30folders.map(folderName => ({

c7906f1a2ca09c4fd9ab2966860d11db73773a35
Sebsterio <sebastian.rosloniec@gmail.com>
2019-12-19 18:29:38 +0100

    bugfix: responsive iframes

--- index.js
+++ index.js
@@ -1,32 +1,45 @@
 // import JS30projects
 // import addProjects (projectsArray, targetContainer, targetNav, iframeSize)
 
-const content = document.getElementById("content");
-const list = document.getElementById("list");
-const windowSize = {
-	width:
-		window.innerWidth ||
-		document.documentElement.clientWidth ||
-		document.body.clientWidth,
-	height:
-		window.innerHeight ||
-		document.documentElement.clientHeight ||
-		document.body.clientHeight
-};
-const tileSize = {
-	width: (function(w) {
-		return w.width > 600 ? 600 : w.width;
-	})(windowSize),
-	height: (function(w) {
-		return w.height > 600 ? 600 : w.height;
-	})(windowSize)
-};
+function updateWindowSize() {
+	return {
+		width:
+			window.innerWidth ||
+			document.documentElement.clientWidth ||
+			document.body.clientWidth,
+		height:
+			window.innerHeight ||
+			document.documentElement.clientHeight ||
+			document.body.clientHeight
+	};
+}
 
-addProjects(JS30projects, content, list, tileSize);
+// fit tile to screen if screen.width < max
+// else, tileSize = max
+function updateTileSize({ width, height }, max) {
+	return width > max
+		? {
+				width: max,
+				height: max
+		  }
+		: {
+				width,
+				height
+		  };
+}
 
-window.addEventListener("resize", () => {
-	// debounce not needed as iframe containers load before their content, so the resize is smooth
+function loadDoc() {
+	const content = document.getElementById("content");
+	const list = document.getElementById("list");
+
+	let tileSize = updateTileSize(updateWindowSize(), 600);
+
 	list.innerHTML = "";
 	content.innerHTML = "";
 	addProjects(JS30projects, content, list, tileSize);
-});
+}
+
+// debounce not needed as iframe containers load before their content, so event re-fires before iframe scripts load
+window.addEventListener("resize", loadDoc);
+
+loadDoc();

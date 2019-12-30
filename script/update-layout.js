a9733ff6b246aed986cef14360fbd8f70cffa48d
Sebsterio <sebastian.rosloniec@gmail.com>
2019-12-30 18:08:36 +0100

    fix and optimize layout change on resize

--- script/update-layout.js
+++ script/update-layout.js
@@ -1,17 +1,19 @@
 (function() {
 	// Minimum screen width for 2-col layout
-	const BOOK_LAYOUT_MIN_WIDTH = 800;
+	const SPLIT_LAYOUT_MIN_WIDTH = 800;
 
 	// iframe width in "mobile screen" layout
 	const NARROW_LAYOUT_WIDTH = 400;
 
+	// px -> num
 	function getContainerSize(container) {
 		return {
-			containerWidth: parseInt(getComputedStyle(container).width),
-			containerHeight: parseInt(getComputedStyle(container).height)
+			width: parseInt(getComputedStyle(container).width),
+			height: parseInt(getComputedStyle(container).height)
 		};
 	}
 
+	// Enable/disable selectEl options at resize boundry
 	function toggleDisabled(selectEl, doDisable) {
 		const option1 = selectEl.querySelector('option[value="1"]');
 		const option2 = selectEl.querySelector('option[value="2"]');
@@ -25,41 +27,25 @@
 		}
 	}
 
-	// ...Will be developed further to take into account user selected layout
-	function getLayout(containerSize, min) {
-		if (containerSize.containerWidth > min) return 1;
-		else return 0;
-	}
-
-	function updateLayoutSelect(layoutSelect, layout) {
-		if (layout > 0) {
-			toggleDisabled(layoutSelect, false);
-			layoutSelect.value = layout;
-		} else {
-			toggleDisabled(layoutSelect, true);
-			layoutSelect.value = 0;
-		}
-	}
-
-	function getPageSizes({ containerWidth, containerHeight }, layout) {
+	function getPageSizes({ width, height }, layout) {
 		let frameWidth, panelWidth;
 		// Full width
 		if (layout === 0) {
-			frameWidth = panelWidth = containerWidth;
+			frameWidth = panelWidth = width;
 		}
 		// Half width
 		else if (layout === 1) {
-			frameWidth = panelWidth = containerWidth / 2;
+			frameWidth = panelWidth = width / 2;
 		}
 		// Narrow width
 		else if (layout === 2) {
 			frameWidth = NARROW_LAYOUT_WIDTH;
-			panelWidth = containerWidth - NARROW_LAYOUT_WIDTH;
+			panelWidth = width - NARROW_LAYOUT_WIDTH;
 		} else new Error("layout type error");
 		return {
 			frameWidth,
 			panelWidth,
-			height: containerHeight
+			height: height
 		};
 	}
 
@@ -82,22 +68,48 @@
 	}
 
 	// Update frame and panel size and layoutSelect options according to screen size
+	let userLayout;
 	window.updateLayout = function(container, selectEl, event) {
 		const containerSize = getContainerSize(container);
-		const newLayout = event
-			? // UI select
-			  Number(event.target.value)
-			: // window resize
-			  getLayout(containerSize, BOOK_LAYOUT_MIN_WIDTH);
-		// TODO: only update layout at boundry or UI select
-		// if (newLayout !== userLayout)
-		// currentLayout = newLayout;
-		const pageSizes = getPageSizes(containerSize, newLayout);
+		let newLayout;
 
-		// On load/resize, enable/disable #layout-select options
-		if (!event) updateLayoutSelect(selectEl, newLayout);
+		// UI select layout
+		if (event) {
+			userLayout = newLayout = Number(event.target.value);
+		}
+		// Window resize | loadDoc()
+		else {
+			newLayout = containerSize.width >= SPLIT_LAYOUT_MIN_WIDTH ? 1 : 0;
+			const currentLayout = Number(selectEl.value);
+			// at resize boundry
+			if (newLayout !== currentLayout) {
+				toggleDisabled(selectEl, !newLayout);
+				if (newLayout && !isNaN(userLayout)) newLayout = userLayout;
+				selectEl.value = newLayout;
+			}
+		}
 
 		// Update page classes (layout) and iframe & panel sizes
+		const pageSizes = getPageSizes(containerSize, newLayout);
 		updatePages(container, newLayout, pageSizes);
 	};
 })();
+
+// JUST IN CASE
+//
+// // Window resize | loadDoc()
+// else {
+// 	// wide screen
+// 	if (containerSize.width >= SPLIT_LAYOUT_MIN_WIDTH) {
+// 		newLayout = 1;
+// 		toggleDisabled(selectEl, false);
+// 		if (!isNaN(userLayout)) newLayout = userLayout;
+// 	}
+// 	// narrow screen
+// 	else {
+// 		newLayout = 0;
+// 		toggleDisabled(selectEl, true);
+// 	}
+// 	// update selectEl if layout changed
+// 	if (newLayout !== currentLayout) selectEl.value = newLayout;
+// }
